206d0446f786e1d624479876743bad37e25d0176
avas3000 <mironkrylov@gmail.com>
2017-09-19 11:48:38 +0300

    another variant

--- hw for 26.09.py
+++ hw for 26.09.py
@@ -1,11 +1,12 @@
 a=int(input("enter 'a' pls "))
 b=int(input("enter 'b' pls "))
 c=int(input("enter 'c' pls "))
+if c==a*b:
+    print (str(a)+"*"+str(b)+"="+str(c))
+else:
+    print (str(a)+"*"+str(b)+"!="+str(c))
 if c==a/b:
     print (str(a)+"/"+str(b)+"="+str(c))
 else:
     print (str(a)+"/"+str(b)+"!="+str(c))
-if c==a**b:
-    print (str(a)+"^"+str(b)+"="+str(c))
-else:
-    print (str(a)+"^"+str(b)+"!="+str(c))
+
