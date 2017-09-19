76ab2fe1cfff19023d72120c5729a4f83a521aa7
avas3000 <mironkrylov@gmail.com>
2017-09-19 11:06:25 +0300

    homework made 19/09/17 for 26/09/17

---
+++ hw for 26.09.py
@@ -0,0 +1,11 @@
+a=int(input("enter 'a' pls "))
+b=int(input("enter 'b' pls "))
+c=int(input("enter 'c' pls "))
+if c==a/b:
+    print (str(a)+"/"+str(b)+"="+str(c))
+else:
+    print (str(a)+"/"+str(b)+"!="+str(c))
+if c==a**b:
+    print (str(a)+"^"+str(b)+"="+str(c))
+else:
+    print (str(a)+"^"+str(b)+"!="+str(c))
